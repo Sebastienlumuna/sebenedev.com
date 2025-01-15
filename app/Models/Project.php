0cb97525368ca8fe7c26b1ca33cf30aa450530f9
Sebastienlumuna <sebastienlumuna8@gmail.com>
2025-01-15 19:16:13 +0100

    mise en place de la relation entre projet et commentaire

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
@@ -21,5 +22,10 @@ class Project extends Model
     public function getRouteKeyName()
     {
         return 'name';
+    }
+
+    public function commentaires(): HasMany
+    {
+        return $this->hasMany(Commentaire::class);
     }
 }
